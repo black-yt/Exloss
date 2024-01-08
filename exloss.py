793176dc3f53bb73d9c26aafaf68b1acd9d874e4
black-yt <2667685528@qq.com>
2024-01-08 15:51:04 +0800

    24.1.8

--- exloss.py
+++ exloss.py
@@ -5,18 +5,18 @@ def Exloss(pred, target):
     mse_loss = torch.mean((pred-target)**2)
 
     N, C, H, W = pred.shape
-    # 得到target中 90% 和 10% 的分位点作为极端最大值和极端最小值的阈值
+    # 得到 target 中 90% 和 10% 的分位点作为极端最大值和极端最小值的阈值，记作 tar_up 和 tar_down
     up_th = 0.9
     down_th = 0.1
     tar_up =  torch.quantile(target.view(N, C, H*W), q=up_th, dim=-1).unsqueeze(-1).unsqueeze(-1) # N,C,1,1
     tar_down =  torch.quantile(target.view(N, C, H*W), q=down_th, dim=-1).unsqueeze(-1).unsqueeze(-1) # N,C,1,1
 
-    target_up_area = F.relu(target-tar_up) # target中大于极端最大值阈值的部分
-    target_down_area = -F.relu(tar_down-target) # target中小于极端最小值阈值的部分
-    pred_up_area = F.relu(pred-tar_up) # pred中大于极端最大值阈值的部分
-    pred_down_area = -F.relu(tar_down-pred) # pred中小于极端最小值阈值的部分
+    target_up_area = F.relu(target-tar_up) # target 中大于 tar_up 的部分
+    target_down_area = -F.relu(tar_down-target) # target 中小于 tar_down 的部分
+    pred_up_area = F.relu(pred-tar_up) # pred 中大于 tar_up 的部分
+    pred_down_area = -F.relu(tar_down-pred) # pred 中小于 tar_down 的部分
 
-    # 对于pred中低估的部分加大loss
+    # 对于 pred 中低估（极大值预测偏小，极小值预测偏大）的部分加大 loss 权重
     lamda_underestimate = 2.5 # 低估时的loss权重，加大惩罚
     lamda_overestimate = 0.5 # 高估时的loss权重
     loss_up = lamda_underestimate*(target_up_area-pred_up_area)*F.relu(target_up_area-pred_up_area)+\
@@ -29,7 +29,7 @@ def Exloss(pred, target):
 
     loss_all = mse_loss + ex_loss
 
-    print("all_loss:", loss_all.item(), "mse_loss:", mse_loss.item(), "ex_loss", ex_loss.item())
+    # print("all_loss:", loss_all.item(), "mse_loss:", mse_loss.item(), "ex_loss", ex_loss.item())
 
     return loss_all
 
